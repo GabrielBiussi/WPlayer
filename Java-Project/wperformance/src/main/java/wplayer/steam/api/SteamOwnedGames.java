6edb4c42856cd41d3dc5609ac2a1dce4f7bef23d
Gabriel <gabriel513bb@gmail.com>
2019-09-25 20:33:28 -0300

    rollback

---
+++ Java-Project/wperformance/src/main/java/wplayer/steam/api/SteamOwnedGames.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wplayer.steam.api;
+
+/**
+ *
+ * @author Sergio
+ */
+public class SteamOwnedGames {
+    
+}
